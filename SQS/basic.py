10d4111c2840223a5e9234266c519d1376809d91
umihico <umihico@users.noreply.github.com>
2019-03-14 19:23:20 +0900

    works when fifo

---
+++ SQS/basic.py
@@ -0,0 +1,18 @@
+import boto3
+import json
+import ppickle
+url=ppickle.load("../config.json")["sqs_name"]
+sqs = boto3.resource('sqs')
+
+body = {"type": "Right", "Action": "Onab"}
+queue = sqs.get_queue_by_name(QueueName=url)
+response = queue.send_message(
+    MessageBody=json.dumps(body),
+    MessageGroupId='messageGroup5')
+import time
+time.sleep(10)
+queue = sqs.get_queue_by_name(QueueName=url)
+for message in queue.receive_messages():
+    print(json.loads(message.body))
+    print('aa')
+    message.delete()
