d49ce01b82c7e6517c9eb2d189e379a853f27738
umihico <umihico@users.noreply.github.com>
2019-03-14 19:24:47 +0900

    works when normal sqs

--- SQS/basic.py
+++ SQS/basic.py
@@ -4,15 +4,13 @@ import ppickle
 url=ppickle.load("../config.json")["sqs_name"]
 sqs = boto3.resource('sqs')
 
-body = {"type": "Right", "Action": "Onab"}
+body = {"type": "Right", "Action": "Ona"}
 queue = sqs.get_queue_by_name(QueueName=url)
 response = queue.send_message(
-    MessageBody=json.dumps(body),
-    MessageGroupId='messageGroup5')
+    MessageBody=json.dumps(body),)
 import time
 time.sleep(10)
 queue = sqs.get_queue_by_name(QueueName=url)
 for message in queue.receive_messages():
     print(json.loads(message.body))
-    print('aa')
     message.delete()
